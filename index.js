c206bf24954db6d22baabf10bf2d4965dfd880ad
matiscava <matiscava@hotmail.com>
2021-09-08 10:52:47 -0300

    clase01_BackEnd
    asasa

---
+++ index.js
@@ -0,0 +1,45 @@
+class Usuario {
+    constructor (nombre,apellido,libros,mascotas){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.libros=libros;
+        this.mascotas=mascotas;
+    }
+    getFullName() {
+        console.log(`Nombre Completo: ${this.nombre} ${this.apellido}`);
+    }
+    addMascota(nombreMascota) {
+        this.mascotas.push(nombreMascota);
+        console.log('Array mascotas: ',this.mascotas);
+    }
+    countMascotas(){
+        console.log('cantidad de mascotas: ',this.mascotas.length);
+    }
+    addBook(titulo,autor){
+        const libroNuevo = {titulo:titulo,autor:autor}
+        this.libros.push(libroNuevo)
+        console.log(`Se agrega el libro '${libroNuevo.titulo}' del autor ${libroNuevo.autor}`);
+    }
+    getBooks(){
+        const tituloLibros = []
+        this.libros.forEach(libro => {
+            tituloLibros.push(libro.titulo)
+        });
+        console.log('Libros: ',tituloLibros);
+    }
+}
+
+const userPrueba = new Usuario ('Cosme','Fulanito',[{titulo:'Harry Potter',autor:'JK Rowling'},{titulo:'La Iliada',autor:'Homero'}],['milanesa','firulais','pepito']);
+
+console.log('Usuario: ',userPrueba);
+
+
+userPrueba.getFullName();
+
+userPrueba.addMascota('Sirius');
+
+userPrueba.countMascotas();
+
+userPrueba.addBook('El Principito','Antoine de Saint-Exupéry');
+
+userPrueba.getBooks();
